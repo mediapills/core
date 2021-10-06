ca8238b2cba8424fb221bed18437019cbcb0683a
mediapills <mediapills@gmail.com>
2021-10-06 22:15:04 +0300

    Added requirements.txt in setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 import setuptools
+from pip._internal.network.session import PipSession
+from pip._internal.req import parse_requirements
 
 version = "0.0.3"
 
-setuptools.setup(version=version)
+requirements = parse_requirements("requirements.txt", session=PipSession())
+
+install_requires = [str(requirement.req) for requirement in requirements]  # type: ignore
+
+setuptools.setup(version=version, install_requires=install_requires)
